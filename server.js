f08d82d1d4d2d425f7f7d7cd522404785e76adbc
trinhnhatminh22 <59037890+trinhnhatminh22@users.noreply.github.com>
2022-03-08 20:52:54 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const FAVORITE_INSTRUCTOR = process.env.NAME;
+
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function main() {
+  while(true) {
+    console.log(` ${FAVORITE_INSTRUCTOR} rock!`);
+    await sleep(5000);
+  }
+}
+
+main();
